02b145f15c933e224bd7de832d7ebaed3dd838ae
DhruvRathod <dhruvrathod195@gmail.com>
2025-06-03 16:37:38 +0530

    Created backend for websocket connection

--- server/index.js
+++ server/index.js
@@ -11,18 +11,32 @@ const wsServer = new WebSocketServer({ server })
 const connections = {}
 const users = {}
 
-const broadcast = () => {
-
+const broadcastUsers = () => {
+    Object.keys(connections).forEach(uuid => {
+        const connection = connections[uuid]
+        const message = JSON.stringify(users)
+        connection.send(message)
+    })
 }
 
 const handleMessage = (bytes, uuid) => {
     const message = JSON.parse(bytes.toString())
     const user = users[uuid]
     user.state = message
-    console.log(message)
+
+    broadcastUsers()
+
+    console.log(`${user.username} updated their state: ${JSON.stringify(user.state)}`)
 
 }
-const handleClose = (uuid) => { }
+const handleClose = (uuid) => {
+
+    console.log(`${users[uuid].username} disconnected`)
+    delete connections[uuid]
+    delete users[uuid]
+
+    broadcastUsers()
+}
 wsServer.on("connection", (connection, request) => {
     // first of all we need to initilaize server protocol ws or wss just like htttp and https 
     //ws://localhost:8000
